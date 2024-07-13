f31f817c8218ce0b3ba224981ed0aeb71cc32fbf
Shubham0085a <aasy0085@gmail.com>
2024-07-13 19:19:03 +0530

    add some offbeat content

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,14 @@ const Header = (props) => (
         <div className="content">
             <div className="inner">
                 <h1>Offbeat Technet</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <p>Offbeat Technet Private Limited <br/>
+                We Work Prestige, Atlanta, 1A Block, Koramangala, Bangalore.</p>
             </div>
         </div>
         <nav>
             <ul>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('projects')}}>Projects</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
